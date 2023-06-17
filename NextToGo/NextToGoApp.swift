edad1c0ad38873bcb8916f685b06194473ce086c
Arinjoy <arinjoy.biswas@hotmail.com>
2023-06-17 11:03:59 +1000

    Restructure root folder without any redundant nested folder

---
+++ NextToGo/NextToGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextToGoApp.swift
+//  NextToGo
+//
+//  Created by Arinjoy Biswas on 17/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct NextToGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
