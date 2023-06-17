5879d72c5b1de1f963a97b1dee1d4eab0ae2238b
Arinjoy <arinjoy.biswas@hotmail.com>
2023-06-17 12:42:32 +1000

    Update project settings  and test file cleanup

--- PresentationLayer/Tests/PresentationLayerTests/PresentationLayerTests.swift
+++ PresentationLayer/Tests/PresentationLayerTests/PresentationLayerTests.swift
@@ -9,17 +9,10 @@ import XCTest
 @testable import PresentationLayer
 
 final class PresentationLayerTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    // TODO:
     func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(1,1)
     }
-
 }
