4ee2c196e75aeb092b4f3fae6c831a5642bbec49
Arinjoy <arinjoy.biswas@hotmail.com>
2023-06-18 20:26:30 +1000

    Add accessibility helpers and string extensions for convenience

---
+++ SharedUtils/Sources/SharedUtils/Accessbility+Helpers.swift
@@ -0,0 +1,30 @@
+//
+//  Created by Arinjoy Biswas on 18/6/2023.
+//
+
+import SwiftUI
+
+public extension View {
+    
+    /// A helper to not let the font scale at too high rate after the larger accessibility font sizes
+    func adaptiveScaleFactor() -> some View {
+        self.modifier(AdaptiveScaleFactor())
+    }
+    
+}
+
+// MARK: - Private
+
+private struct AdaptiveScaleFactor: ViewModifier {
+
+    @Environment(\.sizeCategory) private var sizeCategory
+
+    func body(content: Content) -> some View {
+        if sizeCategory >= .extraExtraExtraLarge {
+            content.minimumScaleFactor(0.5)
+        } else {
+            content.minimumScaleFactor(1)
+        }
+    }
+
+}
---
+++ SharedUtils/Sources/SharedUtils/String+Extensions.swift
@@ -0,0 +1,51 @@
+//
+//  Created by Arinjoy Biswas on 18/6/2023.
+//
+
+import Foundation
+
+///
+/// Helps joining multiple string components to create a combined accessibility label string
+/// with comma separators. Accepts `nil` s for convenience but ignores them. Also trims anything needed
+/// and creates the final result that is a legible sound for VoiceOver.
+///
+/// Example: Input: `["Hello", "", " world", " ", nil]`
+///        Output: `"Hello, World"`
+///
+public extension Array where Element == String? {
+
+    func combinedAccessibilityLabel() -> String {
+        self.compactMap { $0?.trimmed() }
+            .filter { $0.isEmpty == false }
+            .joined(separator: ", ")
+    }
+
+}
+
+public extension String {
+
+    func isEmpty() -> Bool {
+        self.trimmed().lengthOfBytes(using: String.Encoding.utf8) == 0
+    }
+
+    func notEmpty() -> Bool {
+        self.isEmpty() == false
+    }
+    
+    func trimmed() -> String {
+        self.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+    }
+
+}
+
+public extension Optional where Wrapped == String {
+
+    var isEmptyOrNil: Bool {
+        self?.isEmpty ?? true
+    }
+
+    var isNotEmpty: Bool {
+        isEmptyOrNil == false
+    }
+
+}
