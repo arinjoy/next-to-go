705bc544d1643ff7c6719a8eb7aa085765dc48cf
Arinjoy <arinjoy.biswas@hotmail.com>
2023-06-20 16:41:14 +1000

    Add Spy & Mock helpers for Interactor

--- DataLayer/Sources/DataLayer/TestHelpers/TestHelper.swift
+++ DataLayer/Sources/DataLayer/TestHelpers/TestHelper.swift
@@ -7,14 +7,14 @@ import Foundation
 // swiftlint:disable force_try force_unwrapping
 public struct TestHelper {
 
-    static var sampleRacesList: RacesListResponse {
+    public static var sampleRacesList: RacesListResponse {
         return try! JSONDecoder().decode(
             RacesListResponse.self,
             from: TestHelper.jsonData(forResource: "test_races_success")
         )
     }
 
-    static func jsonData(forResource resource: String) -> Data {
+    public static func jsonData(forResource resource: String) -> Data {
         let fileURLPath = Bundle.module.url(forResource: resource,
                                             withExtension: "json",
                                             subdirectory: "JSON/Mocks")
---
+++ DomainLayer/Sources/DomainLayer/TestHelpers/NextRacesInteractor+TestDoubles.swift
@@ -0,0 +1,56 @@
+//
+//  Created by Arinjoy Biswas on 20/6/2023.
+//
+
+import Combine
+import SharedUtils
+import DataLayer
+
+// MARK: - Interactor Spy
+
+public final class NextRacesInteractorSpy: NextRacesInteracting {
+
+    // Spied calls
+    public var nextRacesCalled: Bool = false
+
+    public func nextRaces(
+        for categories: [Race.Category],
+        numberOfRaces count: Int
+    ) -> AnyPublisher<[Race], DataLayer.NetworkError> {
+        nextRacesCalled = true
+        return Empty().eraseToAnyPublisher()
+    }
+}
+
+// MARK: - Interactor Mock
+
+public final class NextRacesInteractorMock: NextRacesInteracting {
+
+    public var returningError: Bool
+    public var error: NetworkError
+    public var resultingData: [Race]
+
+    init(
+        returningError: Bool = false,
+        error: NetworkError = NetworkError.unknown,
+        resultingData: [Race] = NextRacesInteractorMock.sampleRaces
+    ) {
+        self.returningError = returningError
+        self.error = error
+        self.resultingData = resultingData
+    }
+
+    public func nextRaces(
+        for categories: [Race.Category],
+        numberOfRaces count: Int
+    ) -> AnyPublisher<[Race], DataLayer.NetworkError> {
+        if returningError {
+            return .fail(error)
+        }
+        return .just(resultingData).eraseToAnyPublisher()
+    }
+
+    public static var sampleRaces: [Race] {
+        TestHelper.sampleRacesList.races.compactMap { Race(from: $0) }
+    }
+}
